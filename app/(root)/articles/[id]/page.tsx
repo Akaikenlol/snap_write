170f72b653aee8eb0dcef2d1d212470e68476da6
Akaikenlol <keithken972@gmail.com>
2024-08-16 21:36:48 +0630

    feat: Update article page layout and content

--- app/(root)/articles/[id]/page.tsx
+++ app/(root)/articles/[id]/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Page = ({ params }: { params: { id: string } }) => {
-	return <div>{params.id}</div>;
+	return (
+		<div className="home-container">
+			<h1>Article Details</h1>
+			<p>Article Details: {params.id}</p>
+		</div>
+	);
 };
 
 export default Page;
