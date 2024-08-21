3fcfd066df7e1c73b7a95b5d1c5cf1419343aa00
Akaikenlol <keithken972@gmail.com>
2024-08-21 20:28:31 +0630

    feat: Implementing Testimonials Component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Features from "@/components/sections/Features";
 import Header from "@/components/sections/Header";
 import Hero from "@/components/sections/Hero";
 import LogoTicker from "@/components/sections/LogoTicker";
+import Testimonials from "@/components/sections/Testimonials";
 import React from "react";
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
 			<Hero />
 			<LogoTicker />
 			<Features />
+			<Testimonials />
 		</>
 	);
 };
